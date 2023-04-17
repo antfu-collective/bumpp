adf50e2877f5412df5b63c18a652dd84764b9391
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2023-04-17 16:18:10 -0400

    feat: find config in parent directory

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,5 @@
 import { loadConfig } from 'c12'
+import escalade from 'escalade/sync'
 import type { VersionBumpOptions } from './types/version-bump-options'
 
 export const bumpConfigDefaults: VersionBumpOptions = {
@@ -15,16 +16,53 @@ export const bumpConfigDefaults: VersionBumpOptions = {
 
 export async function loadBumpConfig(overrides?: Partial<VersionBumpOptions>,
   cwd = process.cwd()) {
-  const { config } = await loadConfig<VersionBumpOptions>({
-    name: 'bump',
-    defaults: bumpConfigDefaults,
-    overrides: {
-      ...(overrides as VersionBumpOptions),
-    },
-    cwd,
-  })
+  const name = 'bump'
+  const configFile = findConfigFile(name, cwd)
+  if (configFile) {
+    const { config } = await loadConfig<VersionBumpOptions>({
+      configFile,
+      defaults: bumpConfigDefaults,
+      overrides: {
+        ...(overrides as VersionBumpOptions),
+      },
+      cwd,
+    })
 
-  return config!
+    return config!
+  }
+  return structuredClone(bumpConfigDefaults)
+}
+
+function findConfigFile(name: string, cwd: string) {
+  let foundRepositoryRoot = false
+  try {
+    const candidates = ['js', 'mjs', 'ts', 'mts', 'json'].map(ext => `${name}.config.${ext}`)
+    return escalade(cwd, (dir, files) => {
+      const match = files.find((file) => {
+        if (candidates.includes(file))
+          return true
+        if (file === '.git')
+          foundRepositoryRoot = true
+        return false
+      })
+
+      if (match)
+        return match
+
+      // Stop at the repository root.
+      if (foundRepositoryRoot) {
+        // eslint-disable-next-line no-throw-literal
+        throw null
+      }
+
+      return false
+    })
+  }
+  catch (error) {
+    if (foundRepositoryRoot)
+      return null
+    throw error
+  }
 }
 
 export function defineConfig(config: Partial<VersionBumpOptions>) {
