d9dbc8f792e293d01dbcb698f71445c250c9c0d1
Neko Ayaka <neko@ayaka.moe>
2024-10-09 13:28:57 +0800

    chore: use test to create test files
    Signed-off-by: Neko Ayaka <neko@ayaka.moe>

--- test/update-files.test.ts
+++ test/update-files.test.ts
@@ -1,13 +1,23 @@
-import { readFile } from 'node:fs/promises'
+import { mkdir, readFile, rmdir, writeFile } from 'node:fs/promises'
 import { join } from 'node:path'
 import { cwd } from 'node:process'
-import { expect, it } from 'vitest'
+import { afterEach, beforeEach, expect, it } from 'vitest'
 import { Operation } from '../src/operation'
 import { updateFiles } from '../src/update-files'
 
+beforeEach(async () => {
+  mkdir(join(cwd(), 'test', 'update-files', 'testdata'), { recursive: true }).catch(() => { })
+})
+
+afterEach(async () => {
+  rmdir(join(cwd(), 'test', 'update-files', 'testdata'), { recursive: true }).catch(() => { })
+})
+
 it('should skip to modify the manifest file if version field is not specified', async () => {
+  await writeFile(join(cwd(), 'test', 'update-files', 'testdata', 'package.json'), JSON.stringify({}), 'utf8')
+
   const operation = await Operation.start({
-    cwd: join(cwd(), 'test', 'testdata'),
+    cwd: join(cwd(), 'test', 'update-files', 'testdata'),
     currentVersion: '1.0.0',
   })
 
@@ -16,6 +26,6 @@ it('should skip to modify the manifest file if version field is not specified', 
   })
 
   await updateFiles(operation)
-  const updatedPackageJSON = await readFile(join(cwd(), 'test', 'testdata', 'package.json'), 'utf8')
+  const updatedPackageJSON = await readFile(join(cwd(), 'test', 'update-files', 'testdata', 'package.json'), 'utf8')
   expect(JSON.parse(updatedPackageJSON)).toMatchObject({})
 })
