1f81ef38937cee8bc3f6fa6c5fde52b72d2d8374
zcf0508 <zcf0508@live.com>
2026-01-04 14:51:18 +0800

    feat: add prompt to update npm publish config

---
+++ src/publish-config-tag.ts
@@ -0,0 +1,31 @@
+import type { Operation } from './operation'
+import prompts from 'prompts'
+import { prerelease } from 'semver'
+
+export type PublishConfig = Partial<{
+  tag: string
+  access: string
+  provenance: boolean
+  registry: string
+}>
+
+export async function resolvePublishTag(operation: Operation) {
+  const { newVersion } = operation.state
+  const pre = prerelease(newVersion)
+  const defaultTag = pre ? String(pre[0]) : 'latest'
+
+  const response = await prompts({
+    type: 'text',
+    name: 'tag',
+    message: 'Publish Tag',
+    initial: defaultTag,
+  })
+
+  const { tag } = response
+
+  if (tag) {
+    operation.update({
+      publishTag: tag,
+    })
+  }
+}
