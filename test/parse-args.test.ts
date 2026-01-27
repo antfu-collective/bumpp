587a02ba8d306838056cf1c88541e883a82e44de
Oskar Lebuda <oskar.lebuda@enp.pl>
2026-01-27 21:28:17 +0100

    fix tests

--- test/parse-args.test.ts
+++ test/parse-args.test.ts
@@ -66,7 +66,7 @@ describe('loadCliArgs', async () => {
     expect(result.args.sign).toBe(true)
   })
 
-  it('should have configPath property set to the value of the `--configFilePath` flag', () => {
+  it('should have configFilePath property set to the value of the `--configFilePath` flag', () => {
     const result = loadCliArgs([...defaultArgs, '--configFilePath', 'test/fixtures/build.config.ts'])
 
     expect(result.args.configFilePath).toBe('test/fixtures/build.config.ts')
