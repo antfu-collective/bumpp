4ee1a65e88bdb4ee01aba3cd7a45e525c5c6cd58
Kevin Deng <sxzz@sxzz.moe>
2026-03-14 06:44:02 +0800

    refactor

--- src/config.ts
+++ src/config.ts
@@ -1,8 +1,6 @@
 import type { VersionBumpOptions } from './types/version-bump-options'
-import { basename, dirname } from 'node:path'
 import process from 'node:process'
 import { loadConfig } from 'unconfig'
-import escalade from 'escalade/sync'
 
 export const bumpConfigDefaults: VersionBumpOptions = {
   commit: true,
@@ -26,55 +24,26 @@ export async function loadBumpConfig(
 ) {
   const name = 'bump'
   const customPath = overrides?.configFilePath
-  const configFile = customPath || findConfigFile(name, cwd)
-
   const { config } = await loadConfig<VersionBumpOptions>({
     sources: [
-      {
-        files: customPath ? basename(customPath) : `${name}.config`,
-        extensions: customPath ? [] : ['ts', 'mts', 'js', 'mjs', 'json'],
-      },
+      customPath
+        ? {
+            files: [customPath],
+            extensions: [],
+          }
+        : {
+            files: `${name}.config`,
+            extensions: customPath ? [] : ['ts', 'mts', 'js', 'mjs', 'json'],
+          },
     ],
     defaults: bumpConfigDefaults,
-    cwd: configFile ? dirname(configFile) : cwd,
+    cwd,
     merge: false,
   })
 
   return {
     ...config,
-    ...(overrides as VersionBumpOptions),
-  }
-}
-
-function findConfigFile(name: string, cwd: string) {
-  let foundRepositoryRoot = false
-  try {
-    const candidates = ['js', 'mjs', 'ts', 'mts', 'json'].map(ext => `${name}.config.${ext}`)
-    return escalade(cwd, (_dir, files) => {
-      const match = files.find((file) => {
-        if (candidates.includes(file))
-          return true
-        if (file === '.git')
-          foundRepositoryRoot = true
-        return false
-      })
-
-      if (match)
-        return match
-
-      // Stop at the repository root.
-      if (foundRepositoryRoot) {
-        // eslint-disable-next-line no-throw-literal
-        throw null
-      }
-
-      return false
-    })
-  }
-  catch (error) {
-    if (foundRepositoryRoot)
-      return null
-    throw error
+    ...overrides,
   }
 }
 
