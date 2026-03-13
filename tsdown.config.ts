cfa6cd154a1a9b42664539fef06acc36e80ea423
Kevin Deng <sxzz@sxzz.moe>
2026-03-14 06:56:23 +0800

    chore: setup tsgo

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -2,7 +2,9 @@ import { defineConfig } from 'tsdown'
 
 export default defineConfig({
   entry: ['src/index.ts', 'src/cli.ts'],
-  dts: true,
+  dts: {
+    tsgo: true,
+  },
   deps: {
     onlyBundle: [
       'tiny-conventional-commits-parser',
